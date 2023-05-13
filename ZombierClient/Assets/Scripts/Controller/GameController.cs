7f070a6031d2825c8ece4985b8c3579a3d631189
DanilDent <farctate11@gmail.com>
2023-05-13 20:19:46 +0400

    [N-36] add player health bar

--- ZombierClient/Assets/Scripts/Controller/GameController.cs
+++ ZombierClient/Assets/Scripts/Controller/GameController.cs
@@ -1,12 +1,37 @@
+using Prototype.Data;
+using Prototype.Model;
+using Prototype.Service;
 using UnityEngine;
+using Zenject;
 
 namespace Prototype.Controller
 {
     public class GameController : MonoBehaviour
     {
+        [Inject]
+        public void Construct(GameplayEventService eventService, PlayerModel player)
+        {
+            _eventService = eventService;
+            _player = player;
+        }
+
         private void Start()
         {
             Application.targetFrameRate = 60;
+
+            // Init player
+            _player.Health = _player.MaxHealth;
+            // Use this call to update player HealthBar
+            _eventService.OnDamaged(new GameplayEventService.DamagedEventArgs
+            {
+                DamagedEntity = _player,
+                DamageValue = 0f,
+                EntityId = IdData.Empty,
+                IsCrit = false
+            });
         }
+
+        private GameplayEventService _eventService;
+        private PlayerModel _player;
     }
 }
---
+++ ZombierClient/Assets/Scripts/View/PlayerHealthBarUIView.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using Prototype.Model;
+using Prototype.Service;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Prototype.View
+{
+    public class PlayerHealthBarUIView : MonoBehaviour
+    {
+        // Public
+
+        [Inject]
+        public void Construct(GameplayEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        // Private
+
+        // Injected
+        private GameplayEventService _eventService;
+        // From inspector
+        [SerializeField] private Image _imgFiller;
+        [SerializeField] private Image _imgFillerFollower;
+        [SerializeField] private TextMeshProUGUI _text;
+
+        private void OnEnable()
+        {
+            _eventService.Damaged += HandleDamaged;
+        }
+
+        private void OnDisable()
+        {
+            _eventService.Damaged -= HandleDamaged;
+        }
+
+        private void HandleDamaged(object sender, GameplayEventService.DamagedEventArgs e)
+        {
+            if (e.DamagedEntity is PlayerModel player)
+            {
+                _imgFiller.fillAmount = player.Health / player.MaxHealth;
+                float followDuration = 1f;
+                DOTween.To(() => _imgFillerFollower.fillAmount, x => _imgFillerFollower.fillAmount = x, _imgFiller.fillAmount, followDuration);
+
+                _text.SetText($"{(int)player.Health}/{(int)player.MaxHealth}");
+            }
+        }
+    }
+}
